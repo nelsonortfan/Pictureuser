9c6fddd17d12a799349ec39e93675c0bd17d744c
nelsonortfan <n.ortizf@uniandes.edu.co>
2025-06-25 18:04:06 -0500

    agregando cualquier origen a cors

--- src/main/java/com/example/picture/pitcure/config/CorsGlobalConfig.java
+++ src/main/java/com/example/picture/pitcure/config/CorsGlobalConfig.java
@@ -10,7 +10,7 @@ public class CorsGlobalConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // React
+                .allowedOriginPatterns("*") // Permite cualquier origen
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
